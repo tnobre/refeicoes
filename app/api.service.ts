82ce6e93a564af192741570eadb5bd71d9e1a44b
Tiago Nobre <tiago.nobre@gmail.com>
2016-09-23 14:25:23 -0300

    Serviço

---
+++ app/api.service.ts
@@ -0,0 +1,48 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Dia }       from './dia.service';
+import { Observable }     from 'rxjs/Observable';
+
+// Statics
+import 'rxjs/add/observable/throw';
+
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class ApiService {
+  
+  constructor (private http: Http) {}
+  
+  private diasUrl = 'http://localhost:8080/semana';  // URL to web API
+  //private diasUrl = 'http://gturnquist-quoters.cfapps.io/api/random';
+
+  getDias (): Observable<Dia[]> {
+    let headers = new Headers({ 'Content-Type': 'text/plain' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get(this.diasUrl, options)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    //body.data || { };
+    return body || { };
+  }
+  
+  private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+
+}
