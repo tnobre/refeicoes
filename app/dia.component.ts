fea8bf081f7887a734cb5984ebe90fe4611cc83f
Tiago Nobre <tiago.nobre@gmail.com>
2016-10-01 10:54:41 -0300

    inicio do dia component e melhoria layout semana

--- app/dia.component.ts
+++ app/dia.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { Dia } from './dia.service';
 
 @Component({
-	template: `<h2>Refeições do Dia</h2>`
+	templateUrl: 'app/dia.component.html'
 })
 
 export class DiaComponent {
+
+	dia : Dia;
+	chosen : Date;
+	dt : string;
+
+	constructor() {
+		this.chosen = new Date();
+        this.dt = this.chosen.toISOString().slice(0, 10);
+		this.dia = {
+			dia:'hoje', refs : [
+			{ref:'café',state:'active'},
+			{ref:'almoço',state:'active'},
+			{ref:'jantar',state:'active'},
+			]};
+    }
+
+	mudaDia (quantidade : number) {
+		this.chosen = new Date(this.dt);
+		this.chosen.setDate(this.chosen.getDate() + quantidade);
+		this.dt = this.chosen.toISOString().slice(0, 10);
+	}
 
 }
